0e0a3dbd2b5aa7d9dae34761760d5badd0f49584
OleksandrBidylo <161107668+OleksandrBidylo@users.noreply.github.com>
2024-11-23 00:15:44 +0200

    done

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,3 +1,12 @@
-const generateContacts = async (number) => {};
+import { createFakeContact } from '../utils/createFakeContact.js';
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+
+export const generateContacts = async (count) => {
+  const existingContacts = await readContacts();
+  const newContacts = Array.from({ length: count }, createFakeContact);
+  const updatedContacts = [...existingContacts, ...newContacts];
+  await writeContacts(updatedContacts);
+};
 
 generateContacts(5);
--- src/utils/writeContacts.js
+++ src/utils/writeContacts.js
@@ -1,3 +1,10 @@
+import * as fs from 'node:fs/promises';
 import { PATH_DB } from '../constants/contacts.js';
 
-export const writeContacts = async (updatedContacts) => {};
+export const writeContacts = async (contacts) => {
+  try {
+    await fs.writeFile(PATH_DB, JSON.stringify(contacts, null, 2));
+  } catch (error) {
+    console.error('Error writing contacts:', error.message);
+  }
+};
